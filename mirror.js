098dc2ab5d1862ecce6e5a6ddd7ce6c7e6e2937e
Peter Siroki <310489+psiroki@users.noreply.github.com>
2020-03-19 22:54:25 +0100

    Custom facingMode option

--- mirror.js
+++ mirror.js
@@ -3,16 +3,24 @@ var info = document.getElementById("info");
 var flip = false;
 
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({
+  var fm = {
+    "#front": "user",
+    "#back": "environment"
+  };
+  var constraints = {
     video: true,
     width: { min: 320, ideal: 1920 },
-    height: { min: 180, ideal: 1080 } })
-      .then(function (stream) {
-        video.srcObject = stream;
-      })
-      .catch(function (err0r) {
-        console.log("Something went wrong!");
-      });
+    height: { min: 180, ideal: 1080 },
+    facingMode: fm[location.hash]
+  };
+  if (!constraints.facingMode) delete constraints.facingMode;
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!", err0r);
+    });
 }
 
 video.addEventListener("click", function() {
