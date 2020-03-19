7cbe9b34b8eadcf83f89b2dd72bef1138e6ccf8a
Peter Siroki <310489+psiroki@users.noreply.github.com>
2020-03-19 14:21:43 +0100

    Min/ideal resolution and flip option

--- mirror.js
+++ mirror.js
@@ -1,11 +1,20 @@
 var video = document.querySelector("#videoElement");
+var flip = false;
 
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
-    });
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    width: { min: 320, ideal: 1920 },
+    height: { min: 180, ideal: 1080 } })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
 }
+
+video.addEventListener("click", function() {
+  flip = !flip;
+  video.style.transform = flip ? "scale(-1, 1)" : "";
+});
